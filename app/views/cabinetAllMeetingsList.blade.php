b87a9592db3c2b703135edd59026d6526c02f36c
thejeshgn <i@thejeshgn.com>
2014-01-01 21:05:56 +0530

    cabinet meetings

--- app/views/cabinetAllMeetingsList.blade.php
+++ app/views/cabinetAllMeetingsList.blade.php
@@ -26,7 +26,6 @@
     <thead>
         <tr>
             <th> Month/Year </th>
-            <td> No of Meetings</td>
             <th> English </th>
             <th> Kannada </th>
         </tr>
@@ -34,35 +33,30 @@
     <tbody>
             <tr>
                 <td> May 2013 </td>
-                <td> 10</td>
                 <td> <a href="/document/view/cabinet_meeting_13_may_2013_english">View</a></td>
                 <td> &nbsp;</td>
             </tr> 
 
             <tr>
                 <td> Jun 2013 </td>
-                <td></td>                
                 <td> <a href="/document/view/cabinet_meeting_13_jun_2013_english">View</a></td>
                 <td> &nbsp;</td>
             </tr>    
             
             <tr>
                 <td> Jul 2013 </td>
-                <td></td>                
                 <td> <a href="/document/view/cabinet_meeting_17_jul_2013_english">View</a></td>
                 <td> &nbsp;</td>
             </tr>    
             
             <tr>
                 <td> Aug 2013 </td>
-                <td></td>                
                 <td> <a href="/document/view/cabinet_meeting_8_aug_2013_english">View</a></td>
                 <td> &nbsp;</td>
             </tr>    
 
             <tr>
                 <td> Sep 2013 </td>
-                <td></td>
                 <td> <a href="/document/view/cabinet_meeting_6_sep_2013_english">View</a></td>
                 <td> &nbsp;</td>
             </tr>    
